a66c4dd89a22076b2e13662b4a31112eb683f469
ivaldiratavares <83116257+ivaldiratavares@users.noreply.github.com>
2022-06-02 14:28:51 +0100

    Add files via upload

---
+++ api-exame-eletronico/src/main/java/ao/okayulatech/ApiExameEletronicoApplication.java
@@ -0,0 +1,13 @@
+package ao.okayulatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiExameEletronicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiExameEletronicoApplication.class, args);
+	}
+
+}
---
+++ api-exame-eletronico/src/test/java/ao/okayulatech/ApiExameEletronicoApplicationTests.java
@@ -0,0 +1,13 @@
+package ao.okayulatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiExameEletronicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
